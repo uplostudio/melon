eb7eee57eea96afe84e0ff636909b890c09be514
uplostudio <uplostudio@gmail.com>
2023-12-07 20:54:33 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { useState } from "react";
 import AddProduct from "./AddProduct";
 import Auth from "./Auth";
 import "./styles.css";
-
+import Logo from "./jasam.svg";
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false);
 
   return (
     <div className="App">
+      <img src={Logo} />
       {isAuth ? <AddProduct /> : <Auth setIsAuth={setIsAuth} />}
     </div>
   );
